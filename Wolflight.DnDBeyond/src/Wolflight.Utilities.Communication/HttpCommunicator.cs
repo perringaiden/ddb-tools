92a9601b677c269becb092e2dd407dc5046279db
perringaiden <brent.hollett@gmail.com>
2024-10-24 21:39:00 +1000

    Added the IHttpCommunicator interface.

--- Wolflight.DnDBeyond/src/Wolflight.Utilities.Communication/HttpCommunicator.cs
+++ Wolflight.DnDBeyond/src/Wolflight.Utilities.Communication/HttpCommunicator.cs
@@ -2,15 +2,23 @@
 
 namespace Wolflight.Communication
 {
-    public class HttpCommunicator
+    public class HttpCommunicator : IHttpCommunicator
     {
+        /// <summary>
+        /// Creates a new instance of a <see cref="HttpCommunicator"/>.
+        /// </summary>
+        /// <param name="httpClientFactory">The factory to generate <see cref="HttpClient"/> items.</param>
         public HttpCommunicator(IHttpClientFactory httpClientFactory)
         {
             HttpClientFactory = httpClientFactory;
         }
 
+        /// <summary>
+        /// The factory to generate <see cref="HttpClient"/> items.
+        /// </summary>
         private readonly IHttpClientFactory HttpClientFactory;
 
+        /// <inheritdoc/>
         public async Task<JsonDocument> SendGetJsonRequest(
             Uri address,
             IReadOnlyDictionary<string, string> headers
@@ -19,6 +27,7 @@ namespace Wolflight.Communication
             return await JsonDocument.ParseAsync(await SendGetRequest(address, headers));
         }
 
+        /// <inheritdoc/>
         public async Task<Stream> SendGetRequest(
             Uri address,
             IReadOnlyDictionary<string, string> headers
---
+++ Wolflight.DnDBeyond/src/Wolflight.Utilities.Communication/IHttpCommunicator.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Wolflight.Communication
+{
+    /// <summary>
+    /// An interface for making HTTP requests.
+    /// </summary>
+    public interface IHttpCommunicator
+    {
+        /// <summary>
+        /// Make a GET request that returns a <see cref="JsonDocument"/>.
+        /// </summary>
+        /// <param name="address">The address to make the GET request to.</param>
+        /// <param name="headers">The headers to the request.</param>
+        /// <returns>The data if successful.</returns>
+        Task<JsonDocument> SendGetJsonRequest(Uri address, IReadOnlyDictionary<string, string> headers);
+
+        /// <summary>
+        /// Make a GET request that returns a <see cref="Stream"/>.
+        /// </summary>
+        /// <param name="address">The address to make the GET request to.</param>
+        /// <param name="headers">The headers to the request.</param>
+        /// <returns></returns>
+        Task<Stream> SendGetRequest(Uri address, IReadOnlyDictionary<string, string> headers);
+    }
+}

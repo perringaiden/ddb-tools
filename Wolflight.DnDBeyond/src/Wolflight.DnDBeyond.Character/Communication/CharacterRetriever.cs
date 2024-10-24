70cf97181ca66a16c5e145b1a192780f96439ebe
perringaiden <brent.hollett@gmail.com>
2024-10-24 22:56:35 +1000

    Added the character retriever.

---
+++ Wolflight.DnDBeyond/src/Wolflight.DnDBeyond.Character/Communication/CharacterRetriever.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace Wolflight.DnDBeyond.Character.Communication
+{
+    public class CharacterRetriever(Utilities.Communication.IHttpCommunicator communicator) : ICharacterRetriever
+    {
+
+        /// <summary>
+        /// The web address for the character.
+        /// </summary>
+        private const string CharacterFormat = "https://character-service.dndbeyond.com/character/v5/character/{0}?includeCustomItems=true";
+
+
+        /// <summary>
+        /// The HTTP Communicator to use.
+        /// </summary>
+        private Utilities.Communication.IHttpCommunicator Communicator { get; } = communicator;
+
+        /// <inheritdoc/>
+        public async Task<JsonDocument> Retrieve(long id, string token)
+        {
+            Dictionary<string, string> headers = new();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                headers["Authorization"] = $"Bearer {token}";
+            }
+
+
+            return await Communicator.SendGetJsonRequest(new Uri(string.Format(CharacterFormat, id)), headers);
+
+        }
+    }
+}

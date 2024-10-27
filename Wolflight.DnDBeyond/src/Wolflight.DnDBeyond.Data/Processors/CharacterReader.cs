1b6725e4bf788a85534f0b9afffada0bf7985a59
perringaiden <brent.hollett@gmail.com>
2024-10-27 22:16:15 +1000

    Added the shell of a Character Reader.

---
+++ Wolflight.DnDBeyond/src/Wolflight.DnDBeyond.Data/Processors/CharacterReader.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Wolflight.DnDBeyond.Data.Processors
+{
+    /// <summary>
+    /// An implementation to read characters from DnDBeyond.
+    /// </summary>
+    public class CharacterReader : ICharacterReader
+    {
+
+        /// <inheritdoc/>
+        public Types.Character ReadCharacter(JsonDocument characterJson)
+        {
+            return null;
+        }
+
+    }
+}
